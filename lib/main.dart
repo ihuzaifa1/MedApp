0ebc54c911c41676baa6d2d415eb98e974f44b89
Huzaifa Rashid <133530582+ihuzaifa1@users.noreply.github.com>
2024-01-27 17:45:29 +0500

    Integrate Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:medicare_dart/SplashPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'HomePage.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
