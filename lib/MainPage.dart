c64bc20eaf8c074aea60e205ba5d0a64ebe8bfc7
Huzaifa Rashid <133530582+ihuzaifa1@users.noreply.github.com>
2024-02-07 04:33:43 +0500

    MainScreen

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -62,7 +62,7 @@ class MainPage extends StatelessWidget{
                           size: 35,
                         ),
                       ),
-                      SizedBox(height: 40,),
+                      SizedBox(height: 60,),
                       Row(
                         children: [
                           Text("Doctor Appointment",
@@ -116,7 +116,7 @@ class MainPage extends StatelessWidget{
                           size: 35,
                         ),
                       ),
-                      SizedBox(height: 130,),
+                      SizedBox(height: 60,),
                       Row(
                         children: [
                           Text("Profile",
@@ -142,6 +142,121 @@ class MainPage extends StatelessWidget{
 
             ],
           ),
+          SizedBox(height: 10,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 6,
+                          spreadRadius: 4,
+                        )
+                      ]
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle
+                        ),
+                        child: Icon(
+                          Icons.qr_code_scanner,
+                          color: Colors.indigo,
+                          size: 35,
+                        ),
+                      ),
+                      SizedBox(height: 40,),
+                      Row(
+                        children: [
+                          Text("Scan X-Ray & MRI",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 6,
+                          spreadRadius: 4,
+                        )
+                      ]
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle
+                        ),
+                        child: Icon(
+                          Icons.file_copy_sharp,
+                          color: Colors.indigo,
+                          size: 35,
+                        ),
+                      ),
+                      SizedBox(height: 60,),
+                      Row(
+                        children: [
+                          Text("Reports",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+
+            ],
+          )
 
 
         ],
